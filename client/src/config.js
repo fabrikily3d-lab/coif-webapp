fddb4227d915919c0162c65eb2c0e5d33b796d89
Fabrikily3D <fabrikily3d@gmail.com>
2026-02-12 10:06:24 +0100

    Fix API URLs for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,4 @@
-// Update this IP to your computer's local IP address
-const IP_ADDRESS = 'https://coif-webapp-backend.onrender.com';
+const API_BASE = 'https://coif-webapp-backend.onrender.com';
 
-export const API_URL = `http://${IP_ADDRESS}:5000/api`;
-export const SOCKET_URL = `http://${IP_ADDRESS}:5000`;
+export const API_URL = `${API_BASE}/api`;
+export const SOCKET_URL = API_BASE;
